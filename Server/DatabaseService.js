3db14eb65b22462fc48ad6a374fc8fb18ae104a9
Hunter275 <hunter@hunterthornsberry.com>
2025-01-05 22:41:30 -0500

    resolve db file

--- Server/DatabaseService.js
+++ Server/DatabaseService.js
@@ -1,6 +1,6 @@
 const sqlite = require('better-sqlite3');
 const path = require('path');
-const db = new sqlite(path.resolve('database.db'), {fileMustExist: true});
+const db = new sqlite(path.resolve(__dirname + '/database.db'), {fileMustExist: true});
 
 function query(sql, params) {
   if (!params) {
